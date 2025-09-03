049f506db107cc93ba179f042f6a070e6d46e6f3
CarlosCondor99 <ccondorp@pentalab.tech>
2025-09-03 17:31:02 -0500

    [IMP] l10n_ec_credit_card_reconcile

---
+++ l10n_ec_account/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Pentalab Custom Account",
+    'summary': "Pentalab Custom Account module for Ecuador",
+    'description': """
+    This module extends the account payment functionalities to include additional fields and features specific to Ecuadorian accounting practices.
+    """,
+    'author': "PentaLab",
+    'website': "https://pentalab.tech/",
+    'category': 'Accounting',
+    'version': '0.1',
+    'depends': ['account','l10n_ec_credit_card_reconcile'],
+    'data': [
+        'views/account_payment_views.xml',
+        'views/account_card_map_view.xml',
+        'security/ir.model.access.csv',
+        'data/account_cards_data.xml',
+        'views/account_cards_view.xml',
+    ],
+}
+
---
+++ l10n_ec_account/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import account_payment
+from . import account_card_map
+from . import account_cards
