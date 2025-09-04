83cd6a22105252b34d818c5a766cdd12db3dd12e
ViniPenta <apineda@pentalab.tech>
2025-09-04 17:59:16 -0500

    [FIX] duplicado campos pagos y visibilidad en base al tipo de diario

--- l10n_ec_point_of_sale/models/__init__.py
+++ l10n_ec_point_of_sale/models/__init__.py
@@ -3,7 +3,6 @@
 # © 2025 PentaLab
 # License Odoo Proprietary License v1.0 (https://www.odoo.com/documentation/user/16.0/legal/licenses/licenses.html#odoo-proprietary-license)
 
-from . import account_payment
 from . import cash_box
 from . import sale_order
 from . import coins
