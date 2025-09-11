91f3cb7a7992633347199c69bea8795d3dad7572
bbustamantepenta <bbustamante@pentalab.tech>
2025-09-11 09:09:57 -0500

    [IMP] pentalab_report_sale_order_italpisos: cambio de menu padre a Por Facturar

---
+++ l10n_ec_point_of_sale/models/account_payment.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Part of PentaLab. See LICENSE file for full copyright and licensing details.
+# © 2025 PentaLab
+# License Odoo Proprietary License v1.0 (https://www.odoo.com/documentation/user/16.0/legal/licenses/licenses.html#odoo-proprietary-license)
+
+from odoo import models, fields, api, _
+
+
+class FinanceCard(models.Model):
+    _name = 'finance.card'
+    _description = 'Credit/Debit Card'
+    _order = 'name desc'
+    
+    name = fields.Char()
+    card_type = fields.Selection([('debit', 'Debit'), ('credit', 'Credit')], string="Card type")
+
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+    
+    journal_type = fields.Selection(related='journal_id.type', readonly=True, string="Journal Type")
+    bank_reference = fields.Char(string="Bank reference")
+    card = fields.Many2one('finance.card', string="Card")
+    card_payment_type = fields.Selection(
+        [('debit', 'Debit'),
+         ('current', 'Current'),
+         ('deferred_with_interest', 'Deferred with interest'),
+         ('deferred_without_interest', 'Deferred without interest')], string="Payment type"
+    )
+    number_months = fields.Integer(string="Number of months")
+    number_lot = fields.Char()
+    authorization_number = fields.Char(string="Authorization number")
+    bank_id = fields.Many2one("res.partner.bank")
