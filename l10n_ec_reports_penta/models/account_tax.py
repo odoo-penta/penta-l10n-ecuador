4f764474dc42537cd4fde73ed3d78331aab06f2b
ViniPenta <apineda@pentalab.tech>
2025-09-12 11:25:52 -0500

    [UPDATE] avance reporte A2

--- l10n_ec_reports_penta/models/account_tax.py
+++ l10n_ec_reports_penta/models/account_tax.py
@@ -7,3 +7,10 @@ class AccountTaxGroup(models.Model):
 
     show_report = fields.Boolean(default=False, help="If this tax group is checked, it will be considered in the A1 sales report.")
     report_name = fields.Char()
+    type_ret = fields.Selection(
+        [
+            ('withholding_iva_purchase', 'Withholding iva purchase'),
+            ('withholding_iva_sales', 'Withholding iva sales'),
+            ('withholding_rent_purchase', 'Withholding rent purchase'),
+            ('withholding_rent_sales', 'Withholding rent sales'),
+        ], default=False)
