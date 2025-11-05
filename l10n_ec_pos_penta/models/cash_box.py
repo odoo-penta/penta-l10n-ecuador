614addc527f2be046ca07d12e7f7f5568103b661
ViniPenta <apineda@pentalab.tech>
2025-11-05 18:49:01 -0500

    update

--- l10n_ec_pos_penta/models/cash_box.py
+++ l10n_ec_pos_penta/models/cash_box.py
@@ -2,7 +2,6 @@
 # Part of PentaLab. See LICENSE file for full copyright and licensing details.
 # © 2025 PentaLab
 # License Odoo Proprietary License v1.0 (https://www.odoo.com/documentation/user/16.0/legal/licenses/licenses.html#odoo-proprietary-license)
-
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
