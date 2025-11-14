787986b2c19c06cb3132b88d7aba183354fd2468
ViniPenta <apineda@pentalab.tech>
2025-11-14 11:50:25 -0500

    [ADD] 2.1.3 Identificadores para productos de financiamiento

--- l10n_ec_account_penta/__manifest__.py
+++ l10n_ec_account_penta/__manifest__.py
@@ -36,6 +36,7 @@
         'views/account_asset_template_views.xml',
         'views/res_config_settings_views.xml',
         'views/res_partner_views.xml',
+        'views/product_views.xml',
     ],
     'license': 'OPL-1',
 }
--- l10n_ec_account_penta/models/__init__.py
+++ l10n_ec_account_penta/models/__init__.py
@@ -12,3 +12,4 @@ from . import account_asset_template
 from . import res_company
 from . import res_config_settings
 from . import res_partner
+from . import product_template
