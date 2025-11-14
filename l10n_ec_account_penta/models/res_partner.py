931395da0c9afdcd16a6e5b95eb148e602e094e5
ViniPenta <apineda@pentalab.tech>
2025-11-14 11:16:31 -0500

    [ADD] 2.1.2 Agregar perfil del cliente

--- l10n_ec_account_penta/models/res_partner.py
+++ l10n_ec_account_penta/models/res_partner.py
@@ -6,13 +6,22 @@ from odoo import fields, models, _
 class ResPartnerType(models.Model):
     _name = 'res.partner.type'
     
-    name = fields.Char()
+    name = fields.Char(tracking=True)
     active = fields.Boolean(
-        'Active', default=True,
+        'Active', default=True, tracking=True,
         help="By unchecking the active field, you may hide an customer type you will not use.")
+    
+class ResPartnerProfile(models.Model):
+    _name = 'res.partner.profile'
+    
+    name = fields.Char(tracking=True)
+    active = fields.Boolean(
+        'Active', default=True, tracking=True,
+        help="By unchecking the active field, you may hide an customer profile you will not use.")
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     
-    is_customer = fields.Boolean()
-    customer_type = fields.Many2one('res.partner.type')
+    is_customer = fields.Boolean(tracking=True)
+    customer_type = fields.Many2one('res.partner.type', tracking=True)
+    customer_profile = fields.Many2one('res.partner.profile', required=True, tracking=True)
