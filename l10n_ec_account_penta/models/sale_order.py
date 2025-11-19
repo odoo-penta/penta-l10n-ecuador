f0723ca3462b97b118fe98fb3e63372b94dd7a40
ViniPenta <apineda@pentalab.tech>
2025-11-19 12:35:15 -0500

    2.2.5.2.1.2 Proceso de Cálculo  - Paso 1: Separar los Montos de la Venta

--- l10n_ec_account_penta/models/sale_order.py
+++ l10n_ec_account_penta/models/sale_order.py
@@ -10,6 +10,51 @@ class SaleOrder(models.Model):
     months_of_grace = fields.Integer(string='Months of Grace', default=0)
     apply_interest_grace = fields.Boolean(string='Apply Interest Grace', default=False, readonly=True)
     minimum_fee = fields.Monetary(string='Minimum Fee', default=0.0, readonly=True)
+    line_deferred_ids = fields.One2many('sale.order.line.deferred', 'sale_order_id', string='Deferred Lines', readonly=True)
+    
+    def _compute_financing_amounts(self, reward, coupon, **kwargs):
+        """Calcula:
+            - monto_base_financiable
+            - monto_base_contado
+            - iva_financiable
+            - total_sujeto_financiamiento
+        """
+        self.ensure_one()
+        financing_base_amount = 0.0 
+        counted_base_amount = 0.0 
+        financing_iva = 0.0
+        # Obtener productos financiables
+        product_ids = reward.program_id.rule_ids.mapped('valid_product_ids').ids
+        for line in self.order_line:
+            taxes_res = line.tax_id.compute_all(
+                line.price_unit * (1 - (line.discount / 100)),
+                currency=self.currency_id,
+                quantity=line.product_uom_qty,
+                product=line.product_id,
+                partner=self.partner_id
+            )
+            base_amount = taxes_res['total_excluded']
+            iva_amount = sum(t['amount'] for t in taxes_res['taxes'])
+            # Sumar base e impuestos SOLO para las líneas financiables
+            if line.product_id.id in product_ids:
+                financing_base_amount += base_amount
+                financing_iva += iva_amount
+            else:
+                counted_base_amount += base_amount
+        total_subject_financing = financing_base_amount + financing_iva
+        return {
+            'financing_base_amount': financing_base_amount,
+            'counted_base_amount': counted_base_amount,
+            'financing_iva': financing_iva,
+            'total_subject_financing': total_subject_financing,
+        }
+    
+    def calculate_lines_deferred(self, reward, coupon, **kwargs):
+        self.ensure_one()
+        self.line_deferred_ids.unlink()
+        values = self._compute_financing_amounts(reward, coupon, **kwargs)
+        import pdb;pdb.set_trace()
+        global_discount_reward_lines = self._get_applied_global_discount_lines()
 
     def _apply_program_reward(self, reward, coupon, **kwargs):
         self.ensure_one()
@@ -39,7 +84,28 @@ class SaleOrder(models.Model):
             self.apply_interest_grace = reward.apply_interest_grace
             self.minimum_fee = reward.minimum_fee
             self.payment_term_id = reward.apply_payment_terms.id
+            import pdb;pdb.set_trace()
+            self.calculate_lines_deferred(reward, coupon, **kwargs)
         else:
             reward_vals = self._get_reward_line_values(reward, coupon, **kwargs)
             self._write_vals_from_reward_vals(reward_vals, old_reward_lines)
         return {}
+    
+class SaleOrderLineDeferred(models.Model):
+    _name = 'sale.order.line.deferred'
+    
+    sale_order_id = fields.Many2one('sale.order', string='Sale Order')
+    currency_id = fields.Many2one(
+        'res.currency',
+        string='Currency',
+        related='sale_order_id.currency_id',
+        store=True,
+        readonly=True
+    )
+    month = fields.Integer(string='Month')
+    initial_balance = fields.Monetary(string='Initial Balance', currency_field='currency_id')
+    interest_amount = fields.Monetary(string='Interest Amount', currency_field='currency_id')
+    amortization = fields.Monetary(string='Amortization', currency_field='currency_id')
+    final_balance = fields.Monetary(string='Final Balance', currency_field='currency_id')
+    installment = fields.Integer(string='Installment')
+    due_date = fields.Date(string='Due Date')
