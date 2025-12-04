b3b1c9e89f20908e4860cbb27cfb8f430d0bc3e9
ViniPenta <apineda@pentalab.tech>
2025-12-04 10:24:41 -0500

    [FIX] brand in translate

--- l10n_ec_account_penta/__manifest__.py
+++ l10n_ec_account_penta/__manifest__.py
@@ -21,7 +21,7 @@
     ],
     'website': "https://pentalab.tech/",
     'category': 'Accounting',
-    'version': '18.0.0.2',
+    'version': '18.0.0.3',
     'depends': [
         'account_accountant',
         'account_asset',
