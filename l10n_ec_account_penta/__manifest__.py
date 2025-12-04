0846d4d80da1e49c3ee63b5a4fe3b7105cc967e6
ViniPenta <apineda@pentalab.tech>
2025-12-04 10:02:41 -0500

    [FIX] correccion uso de class en xpath

--- l10n_ec_account_penta/__manifest__.py
+++ l10n_ec_account_penta/__manifest__.py
@@ -21,7 +21,7 @@
     ],
     'website': "https://pentalab.tech/",
     'category': 'Accounting',
-    'version': '18.0.0.0',
+    'version': '18.0.0.1',
     'depends': [
         'account_accountant',
         'account_asset',
