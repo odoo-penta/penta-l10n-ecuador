e8763e9782e36b43339708636110135a09f9a8c7
ViniPenta <apineda@pentalab.tech>
2025-12-15 12:08:44 -0500

    [FIX] corregido help de plantilla de activos en configuracion

--- l10n_ec_account_penta/__manifest__.py
+++ l10n_ec_account_penta/__manifest__.py
@@ -21,7 +21,7 @@
     ],
     'website': "https://pentalab.tech/",
     'category': 'Accounting',
-    'version': '18.0.0.6',
+    'version': '18.0.0.0.7',
     'depends': [
         'account_accountant',
         'account_asset',
--- l10n_ec_account_penta/models/res_company.py
+++ l10n_ec_account_penta/models/res_company.py
@@ -7,4 +7,4 @@ class ResCompany(models.Model):
     _inherit = "res.company"
 
     asset_template_id = fields.Many2one('account.asset.template',
-        help='We specify the amount that customer purchases must reach to be considered in the UAFE report.')
+        help='We defined the template to be used for the fixed assets report.')
