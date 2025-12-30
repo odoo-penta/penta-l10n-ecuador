e4fe2a290766c8fcf259f2bf113a343e4c355d6d
ViniPenta <apineda@pentalab.tech>
2025-12-30 17:58:08 -0500

    pdb eliminado

--- l10n_ec_account_penta/models/account_payment_register.py
+++ l10n_ec_account_penta/models/account_payment_register.py
@@ -5,7 +5,6 @@ class AccountPaymentRegister(models.TransientModel):
     _inherit = 'account.payment.register'
     
     def action_create_payments(self):
-        import pdb;pdb.set_trace()
         # Validar que no se pague facturas canceladas
         for line in self.line_ids:
             if line.move_id.state == 'cancel':
