92c239d9151627202b20cad05e6f9f61b79a6401
ViniPenta <apineda@pentalab.tech>
2026-01-06 17:18:21 -0500

    [ADD] modulo l10n_ec_stock_penta

---
+++ l10n_ec_stock_penta/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : PentaLab (<https://pentalab.tech>)
+# Copyright(c): 2025
+# All Rights Reserved.
+#
+# This module is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#################################################################################
+{
+    'name': "Pentalab Custom Stock",
+    'summary': "Pentalab Custom Stock module for Ecuador",
+    'description': """
+    This module extends the stock functionalities to include additional fields and features specific to Ecuadorian inventory practices.
+    """,
+    'author': "PentaLab",
+    'contributors': [
+        'AntonyPineda <vini16.av@gmail.com>'
+    ],
+    'website': "https://pentalab.tech/",
+    'category': 'Inventory/Inventory',
+    'version': '18.0.2.1.0',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_picking_views.xml',
+    ],
+    'license': 'OPL-1',
+}
+
