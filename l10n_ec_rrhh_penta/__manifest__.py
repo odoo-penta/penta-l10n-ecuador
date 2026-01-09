d3a1d6dd00fe7850ca23f6a37a0a2f180676c15d
ViniPenta <apineda@pentalab.tech>
2026-01-09 18:25:53 -0500

    update

--- l10n_ec_rrhh_penta/__manifest__.py
+++ l10n_ec_rrhh_penta/__manifest__.py
@@ -20,7 +20,7 @@
     'website': 'https://pentalab.tech/',
     "license": "LGPL-3",
     'category': 'Human Resources/Employees',
-    "version": "18.0.3.2.0",
+    "version": "18.0.4.0.0",
     "depends": [
         "hr",
         "hr_holidays",
