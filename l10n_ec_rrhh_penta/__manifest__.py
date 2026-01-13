963c441d349cc034616285fcc54181f9ece83171
ViniPenta <apineda@pentalab.tech>
2026-01-13 12:09:21 -0500

    [FIX] correccion palabra mal escrita

--- l10n_ec_rrhh_penta/__manifest__.py
+++ l10n_ec_rrhh_penta/__manifest__.py
@@ -20,7 +20,7 @@
     'website': 'https://pentalab.tech/',
     "license": "LGPL-3",
     'category': 'Human Resources/Employees',
-    "version": "18.0.6.2.1",
+    "version": "18.0.6.2.2",
     "depends": [
         "hr",
         "hr_holidays",
