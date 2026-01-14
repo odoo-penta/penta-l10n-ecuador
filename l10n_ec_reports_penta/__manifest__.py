54a5e19a83fdb6714aabce3e836cd9bf2c4e9cde
ViniPenta <apineda@pentalab.tech>
2026-01-14 17:10:27 -0500

    [FIX] eliminado permisos que ya no deberian estar

--- l10n_ec_reports_penta/__manifest__.py
+++ l10n_ec_reports_penta/__manifest__.py
@@ -21,7 +21,7 @@
     'website': 'https://pentalab.tech/',
     'license': 'OPL-1',
     "category": "Accounting/Accounting",
-    'version': '18.0.0.0.1',
+    'version': '18.0.0.0.2',
     "depends": [
         'account',
         'report_xlsx',
