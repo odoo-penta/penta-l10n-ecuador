2267ff323741563fb64d2179ffd942fd8d9f6893
ViniPenta <apineda@pentalab.tech>
2026-01-16 09:39:38 -0500

    [FIX] traceback

--- l10n_ec_rrhh_penta/models/account_move.py
+++ l10n_ec_rrhh_penta/models/account_move.py
@@ -15,3 +15,13 @@ class AccountMove(models.Model):
         _logger.info("[DEBUG] Se va a crear el asiento")
         _logger.info("TRACE:\n%s","".join(traceback.format_stack()))
         return super().create(vals_list)
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        _logger.info("[DEBUG] Se va a crear linea de asiento")
+        _logger.info("[DEBUG] Valores: %s", vals_list)
+        _logger.info("TRACE:\n%s","".join(traceback.format_stack()))
+        return super().create(vals_list)
