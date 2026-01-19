0bf191e83962956b2091a7d00988902e90c0b349
ViniPenta <apineda@pentalab.tech>
2026-01-19 16:46:36 -0500

    [FIX] correccion dependencia rrhh_penta

--- l10n_ec_rrhh_penta/__manifest__.py
+++ l10n_ec_rrhh_penta/__manifest__.py
@@ -20,13 +20,12 @@
     'website': 'https://pentalab.tech/',
     "license": "LGPL-3",
     'category': 'Human Resources/Employees',
-    "version": "18.0.8.2.1",
+    "version": "18.0.8.2.2",
     "depends": [
         'analytic',
         'hr_contract',
         'hr_payroll_account',
         'hr_payroll_holidays',
-        'module_pentalab_payroll',
     ],
     "data": [
         "data/education_level_data.xml",
