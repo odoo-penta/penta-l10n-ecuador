02987cf21c9d5728afebf8a0d498039c3ef0b08f
ViniPenta <apineda@pentalab.tech>
2026-01-22 09:09:17 -0500

    [FIX] corregido reglas y categorias salariales

--- l10n_ec_rrhh_penta/__manifest__.py
+++ l10n_ec_rrhh_penta/__manifest__.py
@@ -20,7 +20,7 @@
     'website': 'https://pentalab.tech/',
     "license": "LGPL-3",
     'category': 'Human Resources/Employees',
-    "version": "18.0.8.3.0",
+    "version": "18.0.8.3.1",
     "depends": [
         'analytic',
         'hr_contract',
