9ea8984e01a2218e5beaf7efcaed3179d0cbcf42
ViniPenta <apineda@pentalab.tech>
2026-01-26 12:01:40 -0500

    [FIX] ajsute reporte nomina

--- l10n_ec_rrhh_penta/__manifest__.py
+++ l10n_ec_rrhh_penta/__manifest__.py
@@ -20,7 +20,7 @@
     'website': 'https://pentalab.tech/',
     "license": "LGPL-3",
     'category': 'Human Resources/Employees',
-    "version": "18.0.9.0.3",
+    "version": "18.0.9.0.4",
     "depends": [
         'analytic',
         'hr_contract',
