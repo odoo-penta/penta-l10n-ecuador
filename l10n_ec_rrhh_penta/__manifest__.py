7c05ca6c1507c87e55c574c08fd285efe2aa614f
ViniPenta <apineda@pentalab.tech>
2026-01-26 09:25:06 -0500

    [FIX] ajuste reporte recibo de nomina penta

--- l10n_ec_rrhh_penta/__manifest__.py
+++ l10n_ec_rrhh_penta/__manifest__.py
@@ -20,7 +20,7 @@
     'website': 'https://pentalab.tech/',
     "license": "LGPL-3",
     'category': 'Human Resources/Employees',
-    "version": "18.0.9.0.1",
+    "version": "18.0.9.0.2",
     "depends": [
         'analytic',
         'hr_contract',
