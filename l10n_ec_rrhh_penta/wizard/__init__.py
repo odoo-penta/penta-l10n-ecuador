d2219129cad677a947130d660ca7bd542751017a
ViniPenta <apineda@pentalab.tech>
2026-01-27 17:54:36 -0500

    [ADD] reporte de nomina penta pasado de "module_report_hr_payroll"

--- l10n_ec_rrhh_penta/wizard/__init__.py
+++ l10n_ec_rrhh_penta/wizard/__init__.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 from . import add_vacation_wizard
 from . import vacations_bulk_adjust_wizard
 from . import penta_vacation_report_wizard
 from . import import_monthly_inputs_wizard
+from . import payroll_report_wizard
