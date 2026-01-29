8bc444e5cba3bce402e87d465aac521080e1fc79
ViniPenta <apineda@pentalab.tech>
2026-01-29 15:38:29 -0500

    [FIX] escritura de campo y luego recalculo dias tomados vacacionesa

--- l10n_ec_rrhh_penta/__manifest__.py
+++ l10n_ec_rrhh_penta/__manifest__.py
@@ -20,7 +20,7 @@
     'website': 'https://pentalab.tech/',
     "license": "LGPL-3",
     'category': 'Human Resources/Employees',
-    "version": "18.0.9.1.1",
+    "version": "18.0.9.1.2",
     "depends": [
         'analytic',
         'hr_contract',
