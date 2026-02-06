46318ef821ff049aeb1441e90b0ff09f07bd1e4a
ViniPenta <apineda@pentalab.tech>
2026-02-06 11:07:52 -0500

    [FIX] ajuste reporte recibo mostrar horas cantidada

--- l10n_ec_rrhh_penta/__manifest__.py
+++ l10n_ec_rrhh_penta/__manifest__.py
@@ -20,7 +20,7 @@
     'website': 'https://pentalab.tech/',
     "license": "LGPL-3",
     'category': 'Human Resources/Employees',
-    "version": "18.0.10.0.2",
+    "version": "18.0.10.0.3",
     "depends": [
         'analytic',
         'hr_contract',
