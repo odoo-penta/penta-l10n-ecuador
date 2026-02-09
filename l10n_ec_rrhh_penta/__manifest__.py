2af3da98091e1959c882b6419097d440add4aa30
ViniPenta <apineda@pentalab.tech>
2026-02-09 17:56:38 -0500

    [ADD] campo control dias de subsidios en payslip

--- l10n_ec_rrhh_penta/__manifest__.py
+++ l10n_ec_rrhh_penta/__manifest__.py
@@ -20,7 +20,7 @@
     'website': 'https://pentalab.tech/',
     "license": "LGPL-3",
     'category': 'Human Resources/Employees',
-    "version": "18.0.10.1.0",
+    "version": "18.0.10.1.1",
     "depends": [
         'analytic',
         'hr_contract',
