8a905bcc2e8d22bcbcb5fa4b30b008500037c442
ViniPenta <apineda@pentalab.tech>
2026-02-19 12:29:26 -0500

    [ADD] ajuste reglas de subsidios considerar todas en fondos reserva y base subsidios

--- l10n_ec_rrhh_penta/__manifest__.py
+++ l10n_ec_rrhh_penta/__manifest__.py
@@ -20,7 +20,7 @@
     'website': 'https://pentalab.tech/',
     "license": "LGPL-3",
     'category': 'Human Resources/Employees',
-    "version": "18.0.10.2.6",
+    "version": "18.0.10.2.7",
     "depends": [
         'analytic',
         'hr_contract',
