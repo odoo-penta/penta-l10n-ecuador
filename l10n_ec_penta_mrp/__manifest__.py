1a46c2b2a317db4c7c617e3d099e37efcd485828
ViniPenta <apineda@pentalab.tech>
2026-02-24 11:43:11 -0500

    [ADD] modulo l10n_mrp de penta y categoria localizacion penta

---
+++ l10n_ec_penta_mrp/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : PentaLab (<https://pentalab.tech>)
+# Copyright(c): 2025
+# All Rights Reserved.
+#
+# This module is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#################################################################################
+{
+    "name": "Penta Localización MRP",
+    "summary": "Adaptaciones MRP: Configuración de parámetros, Cálculo de costos.",
+    "description": """
+        Adaptaciones MRP: Configuración de parámetros, Cálculo de costos.
+    """,
+    'author': 'PentaLab',
+    'maintainer': 'PentaLab',
+    'contributors': ['AntonyPineda <vini16.av@gmail.com>'],
+    'website': 'https://pentalab.tech/',
+    "license": "LGPL-3",
+    'category': 'Penta Localización Ecuador',
+    "version": "18.0.0.0.0",
+    "depends": [
+        'mrp',
+    ],
+    "data": [
+        
+    ],
+    "installable": True,
+    "application": True,
+    "images": ["static/description/icon.png", "static/description/banner.svg"],
+}
--- l10n_ec_reports_penta/__manifest__.py
+++ l10n_ec_reports_penta/__manifest__.py
@@ -19,8 +19,8 @@
     'maintainer': 'PentaLab',
     'contributors': ['AntonyPineda <vini16.av@gmail.com>'],
     'website': 'https://pentalab.tech/',
-    'license': 'OPL-1',
-    "category": "Accounting/Accounting",
+    "license": "LGPL-3",
+    "category": "Penta Localización Ecuador",
     'version': '18.0.0.0.7',
     "depends": [
         'account',
